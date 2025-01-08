4f4693f269b297c5abf3a94e99eb4e5f7d7b98b4
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-01-08 21:21:19 +0100

    feat: add utility function for showing toasts with customizable types and options

---
+++ client/src/utils/toastUtils.ts
@@ -0,0 +1,25 @@
+import { toast, ToastOptions } from "react-toastify";
+
+export enum ToastType {
+  SUCCESS = "success",
+  ERROR = "error",
+  INFO = "info",
+  WARNING = "warning",
+}
+
+export const showToast = (
+  message: string,
+  type: ToastType = ToastType.SUCCESS,
+  options?: ToastOptions
+) => {
+  toast[type](message, {
+    position: "top-right",
+    autoClose: 2000,
+    style: {
+      backgroundColor: "#282c34",
+      color: "#ffffff",
+      fontFamily: "'Arial', sans-serif",
+    },
+    ...options,
+  });
+};
