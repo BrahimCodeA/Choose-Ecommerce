dd08d76dddfc16b7c7a92859a07e14f7c06b2540
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-01-08 20:56:35 +0100

    feat: add ScrollUp button to return to top of page

---
+++ client/src/components/ScrollUp/ScrollUp.tsx
@@ -0,0 +1,32 @@
+import "./ScrollUp.scss";
+import { useEffect, useState } from "react";
+import { FaArrowUp } from "react-icons/fa";
+
+export default function ScrollUp() {
+  const [showButton, setShowButton] = useState<Boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 530) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`scroll-up ${showButton ? "show-scroll" : "hide-scroll"}`}
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+    >
+      <FaArrowUp />
+    </div>
+  );
+}
