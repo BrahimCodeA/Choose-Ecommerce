49b58232fd0bb6b2230b39e7679dbac64cb05cbb
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-01-08 21:14:13 +0100

    feat: add useProductList hook to fetch and store products

---
+++ client/src/hooks/useProductList.ts
@@ -0,0 +1,25 @@
+import { setProducts } from "@/redux/slices/productsSlice";
+import { RootState } from "@/redux/store";
+import axios from "axios";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+export const useProductList = () => {
+  const products = useSelector((state: RootState) => state.product.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get("/api/product/list");
+        dispatch(setProducts(res.data.products));
+      } catch (error) {
+        console.error("Erreur lors de la récupération des produits :", error);
+      }
+    };
+
+    if (!products || products.length === 0) fetchProducts();
+  }, [dispatch, products]);
+
+  return products;
+};
