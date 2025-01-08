c3840119a72481514a5ab98e20065fb430b93839
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-01-08 21:17:15 +0100

    feat: create Cart component to display empty cart message

---
+++ client/src/pages/Cart/Cart.tsx
@@ -0,0 +1,10 @@
+import "./Cart.scss";
+
+export default function Cart() {
+  return (
+    <div className="cart-container">
+      <h2 className="cart-title">Panier Choose</h2>
+      <p>Votre panier est vide.</p>
+    </div>
+  );
+}
