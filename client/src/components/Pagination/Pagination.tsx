d592ead0151ad7c3ce2ef31106d4e4c3054f4e2b
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-01-10 12:11:16 +0100

    feat(Pagination): add pagination buttons with icons for navigation

---
+++ client/src/components/Pagination/Pagination.tsx
@@ -0,0 +1,41 @@
+import "./Pagination.scss";
+import { Button } from "../ui/Button";
+import React from "react";
+import { ImPrevious2 } from "react-icons/im";
+import { ImNext2 } from "react-icons/im";
+
+interface PaginationProps {
+  page: number;
+  totalPages: number;
+  onPageChange: (newPage: number) => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({
+  page,
+  totalPages,
+  onPageChange,
+}) => {
+  return (
+    <div className="pagination">
+      <Button
+        onClick={() => onPageChange(page - 1)}
+        disabled={page <= 1}
+        className="prev-button"
+      >
+        <ImPrevious2 /> Précédent
+      </Button>
+      <span>
+        Page {page} sur {totalPages}
+      </span>
+      <Button
+        onClick={() => onPageChange(page + 1)}
+        disabled={page >= totalPages}
+        className="next-button"
+      >
+        Suivant <ImNext2 />
+      </Button>
+    </div>
+  );
+};
+
+export default Pagination;
