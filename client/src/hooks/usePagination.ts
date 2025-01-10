deda39495f0f90b5775d6a85dcfe3a3841265fc6
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-01-10 12:10:42 +0100

    feat(pagination): create custom hook for pagination logic

---
+++ client/src/hooks/usePagination.ts
@@ -0,0 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
+import { setPage } from "@/redux/slices/productsSlice";
+import { useMemo } from "react";
+import { RootState } from "@/redux/store";
+
+export const usePagination = <T>(items: T[], pageSize: number) => {
+  const dispatch = useDispatch();
+  const { page } = useSelector((state: RootState) => state.product);
+
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+
+  const itemsToDisplay = useMemo(
+    () => items.slice(startIndex, endIndex),
+    [items, startIndex, endIndex]
+  );
+  const handlePageChange = (newPage: number) => {
+    dispatch(setPage(newPage));
+  };
+  const totalPages = Math.ceil(items.length / pageSize);
+
+  return {
+    itemsToDisplay,
+    page,
+    totalPages,
+    handlePageChange,
+  };
+};
