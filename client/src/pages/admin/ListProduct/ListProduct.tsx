9270ab459e9d86e9fc555f482cddf170c9751ac7
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-01-10 20:43:55 +0100

    refactored: add product list management with pagination and delete functionality

--- client/src/pages/admin/ListProduct/ListProduct.tsx
+++ client/src/pages/admin/ListProduct/ListProduct.tsx
@@ -3,54 +3,68 @@ import { Button } from "@/components/ui/Button";
 import { RiChatDeleteFill } from "react-icons/ri";
 import { useProductList } from "@/hooks/useProductList";
 import { useProductActions } from "@/hooks/useProductActions";
+import Pagination from "@/components/Pagination/Pagination";
+import { usePagination } from "@/hooks/usePagination";
 
 export default function ListProduct() {
   const products = useProductList();
   const { removeProduct } = useProductActions();
+  const { itemsToDisplay, page, totalPages, handlePageChange } = usePagination(
+    products,
+    8
+  );
 
   return (
-    <div className="product-list">
-      <h2>Liste des produits</h2>
-      {products.length > 0 ? (
-        <table className="product-table">
-          <thead>
-            <tr>
-              <th>Nom</th>
-              <th>Catégorie</th>
-              <th>Images</th>
-              <th className="actions">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <tr key={product._id}>
-                <td>{product.name}</td>
-                <td>{product.category}</td>
-                <td className="product-image">
-                  {Array.isArray(product.image) &&
-                    product.image.map((img, index) => (
-                      <img
-                        key={index}
-                        src={img}
-                        alt={`${product.name} image ${index + 1}`}
-                        className="product-thumbnail"
-                      />
-                    ))}
-                </td>
-                <td className="actions-delete">
-                  <Button
-                    className="delete-button"
-                    onClick={() => removeProduct(product._id)}
-                    icon={<RiChatDeleteFill />}
-                  />
-                </td>
+    <>
+      <div className="product-list">
+        <h2>Liste des produits</h2>
+        <h5>Nombre de produits: {products.length}</h5>
+        {products.length > 0 ? (
+          <table className="product-table">
+            <thead>
+              <tr>
+                <th>Nom</th>
+                <th>Catégorie</th>
+                <th>Images</th>
+                <th className="actions">Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p className="no-products">Aucun produit disponible</p>
-      )}
-    </div>
+            </thead>
+            <tbody>
+              {itemsToDisplay.map((product) => (
+                <tr key={product._id}>
+                  <td>{product.name}</td>
+                  <td>{product.category}</td>
+                  <td className="product-image">
+                    {Array.isArray(product.image) &&
+                      product.image.map((img, index) => (
+                        <img
+                          key={index}
+                          src={img}
+                          alt={`${product.name} image ${index + 1}`}
+                          className="product-thumbnail"
+                        />
+                      ))}
+                  </td>
+                  <td className="actions-delete">
+                    <Button
+                      className="delete-button"
+                      onClick={() => removeProduct(product._id)}
+                      icon={<RiChatDeleteFill />}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p className="no-products">Aucun produit disponible</p>
+        )}
+        <Pagination
+          page={page}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    </>
   );
 }
