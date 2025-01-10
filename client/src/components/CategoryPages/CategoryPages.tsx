3dcfe672b7cca62bec39a3539603a85c6034a1e5
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-01-10 12:48:00 +0100

    feat(CategoryPages): use Category component for different categories

--- client/src/components/CategoryPages/CategoryPages.tsx
+++ client/src/components/CategoryPages/CategoryPages.tsx
@@ -1,39 +1,33 @@
 import "./CategoryPages.scss";
-import nike from "@/assets/Home-BS.png";
+import men from "@/assets/Home-Men.jpg";
+import women from "@/assets/Home-Women.avif";
+import kid from "@/assets/Home-Kid.jpg";
+import Category from "./Category";
 
 export default function CategoryPages() {
   return (
     <section className="category-pages">
-      <div className="category">
-        <img src={nike} alt="Sneakers Homme" />
-        <div className="overlay">
-          <h2>Homme</h2>
-          <p>Jordan, Nike, et plus.</p>
-          <a href="/Men" className="btn">
-            Shop
-          </a>
-        </div>
-      </div>
-      <div className="category">
-        <img src={nike} alt="Sneakers Femme" />
-        <div className="overlay">
-          <h2>Femme</h2>
-          <p>Styles tendance.</p>
-          <a href="/Women" className="btn">
-            Shop
-          </a>
-        </div>
-      </div>
-      <div className="category">
-        <img src={nike} alt="Sneakers Enfant" />
-        <div className="overlay">
-          <h2>Enfant</h2>
-          <p>Pour les petits fans.</p>
-          <a href="/Kids" className="btn">
-            Shop
-          </a>
-        </div>
-      </div>
+      <Category
+        imageSrc={men}
+        altText="Sneakers Homme"
+        title="Homme"
+        description="Jordan, Nike, et plus."
+        link="/Men"
+      />
+      <Category
+        imageSrc={women}
+        altText="Sneakers Femme"
+        title="Femme"
+        description="Styles tendance."
+        link="/Women"
+      />
+      <Category
+        imageSrc={kid}
+        altText="Sneakers Enfant"
+        title="Enfant"
+        description="Pour les petits fans."
+        link="/Kid"
+      />
     </section>
   );
 }
