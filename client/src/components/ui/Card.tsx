76f8af52c8e5b2a781eaf9b0c5f94db7d5193c30
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-01-15 11:31:50 +0100

    refactored: add promo and discountPrice for more reusability

--- client/src/components/ui/Card.tsx
+++ client/src/components/ui/Card.tsx
@@ -1,11 +1,12 @@
 import "@/style/Card.scss";
+import { MdDiscount } from "react-icons/md";
 
 type CardProps = {
   image: string;
   title: string;
   price: number | string;
-  discountPrice?: string;
-  promo?: string | JSX.Element;
+  discountPrice?: string | null;
+  promo?: boolean;
 };
 
 export const Card = ({
@@ -18,14 +19,18 @@ export const Card = ({
   return (
     <div className="product-card">
       <img src={image} alt={title} className="card-image" />
-      {promo && <span className="promo-label">{promo}</span>}
+      {promo && (
+        <span className="promo-label">
+          En promo <MdDiscount />
+        </span>
+      )}
       <h3 className="card-name">{title}</h3>
       <div className="card-price">
         <span className={`original-price ${discountPrice ? "has-promo" : ""}`}>
-          {price}
+          {price}€
         </span>
         {discountPrice && (
-          <span className="discount-price">{discountPrice}</span>
+          <span className="discount-price">{discountPrice}€</span>
         )}
       </div>
     </div>
