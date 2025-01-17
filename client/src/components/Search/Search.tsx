a648166b614d558c69af525e3c4856a9e1886518
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-01-17 23:18:32 +0100

    refactor: improve Search component structure and reusability

--- client/src/components/Search/Search.tsx
+++ client/src/components/Search/Search.tsx
@@ -4,11 +4,13 @@ import { Input } from "../ui/Input";
 
 export default function Search({
   onCloseSearch,
+  openSearch,
 }: {
   onCloseSearch: () => void;
+  openSearch: boolean;
 }) {
   return (
-    <div className="search-container show">
+    <div className={`search-container ${openSearch ? "show" : ""}`}>
       <div className="header">
         <h2 className="logo">CHOOSE</h2>
         <Button onClick={onCloseSearch}>Fermer</Button>
