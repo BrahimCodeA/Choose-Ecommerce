a0ede25eb6c78b56d75bbae0424ceb135256f7b3
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-01-26 22:56:37 +0100

    refactor: simplify filter component logic for better maintainability

--- client/src/components/FilterProductMenu/FilterProductMenu.tsx
+++ client/src/components/FilterProductMenu/FilterProductMenu.tsx
@@ -1,41 +1,70 @@
 import "./FilterProductMenu.scss";
 import { Button } from "../ui/Button";
+import { Input } from "../ui/Input";
+import { Select } from "../ui/Select";
+import { Checkbox } from "../ui/Checkbox";
+import { Label } from "../ui/Label";
+import { useState } from "react";
 
 export const FilterProductMenu = ({
   onClose,
   onPromoFilterChange,
   onSearchChange,
+  onSortChange,
 }: {
   onClose: () => void;
   onPromoFilterChange: (isChecked: boolean) => void;
   onSearchChange: (searchQuery: string) => void;
+  onSortChange: (sortOrder: "Croissant" | "Décroissant") => void;
 }) => {
+  const [selectedSortOrder, setSelectedSortOrder] = useState("");
   const handleChecked = (event: React.ChangeEvent<HTMLInputElement>) =>
     onPromoFilterChange(event.target.checked);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) =>
     onSearchChange(event.target.value);
 
+  const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newSortOrder = event.target.value;
+    setSelectedSortOrder(newSortOrder);
+    onSortChange(newSortOrder as "Croissant" | "Décroissant");
+  };
+
   return (
     <div className="filter-product-menu">
-      <h2>Les Filtres</h2>
-      <Button className="filter-close-button" onClick={onClose}>
-        Fermer
-      </Button>
+      <h2>CHOOSE</h2>
+      <Button
+        className="filter-close-button"
+        onClick={onClose}
+        title={"Fermer"}
+      />
 
-      <input
+      <Input
         className="filter-input"
         type="text"
-        placeholder="Rechercher"
+        placeholder="Recherchez une sneakers..."
         onChange={handleSearch}
       />
 
-      <div className="filter-checkboxes">
-        <div className="filter-checkbox">
-          <input type="checkbox" id="promo-checkbox" onChange={handleChecked} />
-          <label htmlFor="promo-checkbox">En Promo</label>
-        </div>
+      <div className="filter-checkbox">
+        <Checkbox
+          type="checkbox"
+          id="promo-checkbox"
+          onChange={handleChecked}
+        />
+        <Label className="promo-checkbox" title={"En Promo"} />
       </div>
+
+      <Select
+        className="filter-select"
+        placeholder="Trier par"
+        value={selectedSortOrder}
+        options={[
+          { value: "Croissant", label: "Croissant" },
+          { value: "Décroissant", label: "Décroissant" },
+        ]}
+        onChange={handleSortChange}
+      />
     </div>
   );
 };
