36f06d028636bd3daf4322345d4cf0574aa45c39
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-02-06 21:49:20 +0100

    feat: Create custom hook for fetching product details

---
+++ client/src/hooks/useProductDetails.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setSelectedProduct } from "@/redux/slices/productsSlice";
+
+const useProductDetails = (id: string) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const res = await axios.get(`/api/product/single/${id}`);
+        dispatch(setSelectedProduct(res.data.product));
+      } catch (error) {
+        console.log(error);
+        dispatch(setSelectedProduct(null));
+      }
+    };
+
+    if (id) fetchProduct();
+  }, [dispatch, id]);
+};
+
+export default useProductDetails;
