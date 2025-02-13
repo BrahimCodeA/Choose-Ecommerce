bcded1cbc11f83efe5ba1364b56d65ee1fefc542
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-02-14 00:46:44 +0100

    feat(Search): add product search functionality

--- client/src/components/Search/Search.tsx
+++ client/src/components/Search/Search.tsx
@@ -1,5 +1,7 @@
 import "./Search.scss";
 import { Button } from "../ui/Button";
+import { ProductSearch } from "@/components/ProductSearch/ProductSearch";
+import { useState } from "react";
 import { Input } from "../ui/Input";
 
 export default function Search({
@@ -9,17 +11,33 @@ export default function Search({
   onCloseSearch: () => void;
   openSearch: boolean;
 }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
     <div className={`search-container ${openSearch ? "show" : ""}`}>
       <div className="header">
         <h2 className="logo">CHOOSE</h2>
         <Button onClick={onCloseSearch}>Fermer</Button>
       </div>
-      <Input type="text" placeholder="Rechercher" />
 
-      <div>
-        <p>Les recherches les plus populaires...</p>
-      </div>
+      <Input
+        type="text"
+        placeholder="Rechercher"
+        value={searchQuery}
+        onChange={handleSearchChange}
+      />
+      {searchQuery.trim() === "" ? (
+        <p>Votre sneakers de rêves ici...</p>
+      ) : (
+        <ProductSearch
+          searchQuery={searchQuery}
+          onCloseSearch={onCloseSearch}
+        />
+      )}
     </div>
   );
 }
