9a7a6e7ef04420d20b1f075f2634c361826fa72a
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-02-19 09:05:26 +0100

    chore: set up Redux store with persistence and routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,7 +10,7 @@ import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate loading={<div>Chargement...</div>} persistor={persistor}>
+      <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
