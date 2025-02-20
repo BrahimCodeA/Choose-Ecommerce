4e8e55d3c2e5830372ba0ffb530d8a8d30edf6a0
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-02-20 22:16:02 +0100

    refactor: simplify structure and improve readability of Hero component

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -1,21 +1,18 @@
 import "./Hero.scss";
-import { Model } from "../Model3D/Model";
+import { Model_One } from "../Model3D/Model_One";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
 export default function Hero() {
   return (
-    <>
-      <Canvas
-        style={{
-          height: "100vh",
-          width: "100wh",
-        }}
-      >
-        <OrbitControls />
-        <ambientLight intensity={10} />
-        <Model />
-      </Canvas>
-    </>
+    <div className="hero-container">
+      <h1 className="hero-title">Elevate Your Look</h1>
+      <div className="canvas-container">
+        <Canvas camera={{ position: [1, 2, 2] }}>
+          <OrbitControls enableZoom={false} enablePan={false} />
+          <Model_One scale={[13, 13, 13]} position={[0, -1, 0]} />
+        </Canvas>
+      </div>
+    </div>
   );
 }
