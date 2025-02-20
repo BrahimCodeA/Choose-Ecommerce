6d24252bf216e053fec6833b94b7e9f5157d069d
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-02-20 18:13:24 +0100

    changes name HeroComponent to Hero for more visuability

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,13 +1,13 @@
 import "./Home.scss";
-import HeroComponent from "@/components/Hero/Hero";
 import { BestSellerSlider } from "@/components/BestSellerSlider/BestSellerSlider";
 import { DiscountedProducts } from "@/components/DiscountedProducts/DiscountedProducts";
 import CategoryPages from "@/components/CategoryLinks/CategoryLinks";
+import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
   return (
     <>
-      <HeroComponent />
+      <Hero />
       <BestSellerSlider />
       <div className="bestseller-discount-separator">
         <div className="text">Une collection unique</div>
