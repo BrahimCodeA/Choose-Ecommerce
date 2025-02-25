e36dec98f5c40ffc9aad4b19df9ccc4a48111926
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-02-25 18:52:35 +0100

    refactored: the server

--- server/server.js
+++ server/server.js
@@ -30,12 +30,4 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-  });
-}
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
