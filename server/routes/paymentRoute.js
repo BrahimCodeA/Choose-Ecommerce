846002679605f652c3fe063c74252b4f0d354c0b
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2025-02-26 16:11:51 +0100

    feat: add protectRoute middleware

--- server/routes/paymentRoute.js
+++ server/routes/paymentRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { createPaymentIntent } from "../controllers/paymentController.js";
+import { protectRoute } from "../middlewares/Auth.js";
 
 const paymentRouter = express.Router();
 
-paymentRouter.post("/create-payment-intent", createPaymentIntent);
+paymentRouter.post("/create-payment-intent", protectRoute, createPaymentIntent);
 
 export default paymentRouter;
